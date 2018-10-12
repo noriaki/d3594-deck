2c59e5ec6c810ede365c14acb363a23643694b92
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-10-12 17:57:58 +0900

    add import method of tactics

--- scripts/replaceDbData.js
+++ scripts/replaceDbData.js
@@ -2,23 +2,38 @@ const { readdirSync, readFileSync } = require('fs');
 const { resolve } = require('path');
 const { connect, disconnect } = require('../server/db');
 const Commander = require('../server/models/commander');
+const Tactics = require('../server/models/tactics');
 
 const logAndExit = (error) => { console.error(error); process.exit(1); };
 
-const main = async () => {
-  await connect();
-  const dirpath = resolve(Commander.getDataPath());
+const getAllData = (modelClass) => {
+  const dirpath = resolve(modelClass.getDataPath());
   const filenames = readdirSync(dirpath);
   const correctFilename = /^[0-9a-f]+\.json$/;
-  const jsons = filenames.filter(
+  return filenames.filter(
     filename => correctFilename.test(filename)
-  ).map(filename => JSON.parse(readFileSync(resolve(dirpath, filename))));
+  ).map(
+    filename => JSON.parse(readFileSync(resolve(dirpath, filename)))
+  );
+};
+
+const main = async () => {
+  await connect();
+
+  // Commander
+  const commanderData = getAllData(Commander);
   await Commander.deleteMany({});
-  await Commander.importAll(jsons);
+  await Commander.importAll(commanderData);
+
+  // Tactics
+  const tacticsData = getAllData(Tactics);
+  await Tactics.deleteMany({});
+  await Tactics.importAll(tacticsData);
+
   await disconnect();
 };
 
 main().then(() => process.exit()).catch(logAndExit);
 
 // error handling
-process.on('unhandledRejection', logAndExit);;
+process.on('unhandledRejection', logAndExit);
