357959513719becda81c105652a5c8c0a1919b39
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-10-18 01:31:16 +0900

    add TEST_SUITE to resolving db-uri

--- server/db/helpers/__tests__/resolveDbUri.test.js
+++ server/db/helpers/__tests__/resolveDbUri.test.js
@@ -2,16 +2,22 @@ import resolve from '../resolveDbUri';
 
 describe('db helpers: resolving DB URI', () => {
   let orgMONGODBURI;
+  let orgNODEENV;
+  let orgTESTSUITE;
 
   beforeEach(() => {
     orgMONGODBURI = process.env.MONGODB_URI;
     delete process.env.MONGODB_URI;
+    orgNODEENV = process.env.NODE_ENV;
+    delete process.env.NODE_ENV;
+    orgTESTSUITE = process.env.TEST_SUITE;
+    delete process.env.TEST_SUITE;
   });
 
   afterEach(() => {
-    if (orgMONGODBURI != null) {
-      process.env.MONGODB_URI = orgMONGODBURI;
-    }
+    if (orgMONGODBURI != null) { process.env.MONGODB_URI = orgMONGODBURI; }
+    if (orgNODEENV != null) { process.env.NODE_ENV = orgNODEENV; }
+    if (orgTESTSUITE != null) { process.env.TEST_SUITE = orgTESTSUITE; }
   });
 
   describe('when setting `process.env.MONGODB_URI`', () => {
@@ -26,23 +32,11 @@ describe('db helpers: resolving DB URI', () => {
       process.env.MONGODB_URI = expected;
       expect(resolve('test')).toBe(expected);
       expect(resolve('test', 'test')).toBe(expected);
+      expect(resolve('test', 'test', 'test-suite')).toBe(expected);
     });
   });
 
   describe('when NODE_ENV specified (development|test|production)', () => {
-    let orgNODEENV;
-
-    beforeEach(() => {
-      orgNODEENV = process.env.NODE_ENV;
-      delete process.env.NODE_ENV;
-    });
-
-    afterEach(() => {
-      if (orgNODEENV != null) {
-        process.env.NODE_ENV = orgNODEENV;
-      }
-    });
-
     it('resolving dbname defaults `development`', () => {
       const expected = 'mongodb://localhost:27017/dbname_development';
       expect(resolve('dbname')).toBe(expected);
@@ -62,6 +56,33 @@ describe('db helpers: resolving DB URI', () => {
       process.env.NODE_ENV = 'production';
       expected = 'mongodb://localhost:27017/dbname_production';
       expect(resolve('dbname')).toBe(expected);
+    });
+
+    describe('and TEST_SUITE specified', () => {
+      it('add to dbname suffix', () => {
+        const expected = 'mongodb://localhost:27017/dbname_test-test-suite';
+        process.env.NODE_ENV = 'test';
+        process.env.TEST_SUITE = 'test-suite';
+        expect(resolve('dbname')).toBe(expected);
+      });
+
+      it('only works NODE_ENV === test', () => {
+        let expected;
+
+        process.env.TEST_SUITE = 'test-suite';
+
+        process.env.NODE_ENV = 'development';
+        expected = 'mongodb://localhost:27017/dbname_development';
+        expect(resolve('dbname')).toBe(expected);
+
+        process.env.NODE_ENV = 'test';
+        expected = 'mongodb://localhost:27017/dbname_test-test-suite';
+        expect(resolve('dbname')).toBe(expected);
+
+        process.env.NODE_ENV = 'production';
+        expected = 'mongodb://localhost:27017/dbname_production';
+        expect(resolve('dbname')).toBe(expected);
+      });
     });
   });
 
