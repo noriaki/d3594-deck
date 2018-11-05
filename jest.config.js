647a1b84706a09910a5623ce587c7534b5670d80
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-11-05 10:02:36 +0900

    add enzyme packages and config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
+const pathIgnorePatterns = [
+  '<rootDir>/.git/',
+  '<rootDir>/.next/',
+  '<rootDir>/node_modules/',
+];
+
 module.exports = {
   setupTestFrameworkScriptFile: './jest.setup.js',
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: pathIgnorePatterns,
+  watchPathIgnorePatterns: pathIgnorePatterns,
 };
