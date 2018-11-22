f71fb6cee2eaaa5e29fa5ae5b5393b42f275445b
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-11-22 09:35:07 +0900

    add mode for saerching 'commanders' and 'tactics'

--- components/DeckEditor/index.js
+++ components/DeckEditor/index.js
@@ -16,35 +16,44 @@ const styles = theme => ({
   },
 });
 
+const SEARCH_COMMANDER_MODE = 'searchCommander';
+const SEARCH_TACTICS_MODE = 'searchTactics';
+
 export class DeckEditorComponent extends Component {
   state = {
-    mode: null, // enum [null, 'searchCommander', 'searchTactics']
-    openSearcher: true,
+    // mode: null, // enum [null, SEARCH_COMMANDER_MODE, SEARCH_TACTICS_MODE]
+    mode: SEARCH_COMMANDER_MODE,
   }
 
-  toggleSearcher = open => () => {
-    let nextOpen = open;
-    const { openSearcher } = this.state;
-    if (nextOpen === undefined) { nextOpen = !openSearcher; }
-    this.setState({
-      openSearcher: nextOpen,
-    });
+  toggleCommanderSearcher = toOpen => () => {
+    let nextMode = toOpen;
+    const { mode: currentMode } = this.state;
+    if (nextMode === undefined) {
+      nextMode = (
+        currentMode === SEARCH_COMMANDER_MODE ? null : SEARCH_COMMANDER_MODE
+      );
+    } else {
+      nextMode = (toOpen === true ? SEARCH_COMMANDER_MODE : null);
+    }
+    this.setState({ mode: nextMode });
   }
 
   render() {
     const { classes } = this.props;
-    const { openSearcher } = this.state;
+    const { mode } = this.state;
     return (
       <Fragment>
         <div className={classes.container}>
-          <Button variant="outlined" onClick={this.toggleSearcher()}>
-            {openSearcher ? 'Close' : 'Open'}
+          <Button
+            variant="outlined"
+            onClick={this.toggleCommanderSearcher()}>
+            {mode === SEARCH_COMMANDER_MODE ? 'Close' : 'Open'}
           </Button>
         </div>
         <CommanderSearcher
-          open={openSearcher}
-          onClose={this.toggleSearcher(false)}
-          onOpen={this.toggleSearcher(true)} />
+          open={mode === SEARCH_COMMANDER_MODE}
+          onClose={this.toggleCommanderSearcher(false)}
+          onOpen={this.toggleCommanderSearcher(true)} />
       </Fragment>
     );
   }
