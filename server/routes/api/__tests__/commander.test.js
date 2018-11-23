0341899ada69e1d6e73cfdc67a30ef84738f47a4
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-11-24 00:06:59 +0900

    fix search results to [] when no filter

--- server/routes/api/__tests__/commander.test.js
+++ server/routes/api/__tests__/commander.test.js
@@ -23,6 +23,7 @@ describe('Routes: `/c`', () => {
   });
 
   describe('search', () => {
+    let query;
     beforeEach(async () => {
       const files = [
         '0022cae0ffb0ee3d8fce63d6d8cdc69f', // 蒋琬,蜀,歩,3
@@ -40,10 +41,19 @@ describe('Routes: `/c`', () => {
         return JSON.parse(readFileSync(dataPath));
       });
       await Commander.importAll(data);
+      query = {
+        text: '',
+        filter: {
+          rarity: [5, 4, 3],
+          army: ['弓', '歩', '騎'],
+          team: ['群', '漢', '魏', '蜀', '呉'],
+        },
+      };
     });
 
     it('returning valid object shape', async () => {
-      const options = { qs: { text: '周瑜' }, json: true };
+      query.text = '周瑜';
+      const options = { qs: query, json: true };
       const expectedKeys = ['_id', 'identifier', 'name', 'imageURL'].sort();
       const subjects = await server.get('/c', options);
       expect(subjects).toHaveLength(1);
@@ -52,17 +62,8 @@ describe('Routes: `/c`', () => {
     });
 
     it('retrieve by text search (column `name`)', async () => {
-      const options = {
-        qs: {
-          text: '張',
-          // filter: {
-          //   rarity: [5, 4, 3, 2, 1],
-          //   army: ['弓', '歩', '騎'],
-          //   team: ['群', '漢', '魏', '蜀', '呉'],
-          // },
-        },
-        json: true,
-      };
+      query.text = '張';
+      const options = { qs: query, json: true };
       const expectedIds = [
         'a9f7deacd67d08a2ea7ff2d4255b4171', // 張角,群,騎,5
         'c78a48266ec5166844c7df427834a520', // 張春華,魏,弓,5
@@ -79,14 +80,8 @@ describe('Routes: `/c`', () => {
     });
 
     it('retrieve by `rarity` filter', async () => {
-      const options = {
-        qs: {
-          filter: {
-            rarity: [4, 3],
-          },
-        },
-        json: true,
-      };
+      query.filter.rarity = [4, 3];
+      const options = { qs: query, json: true };
       const expectedIds = [
         '0022cae0ffb0ee3d8fce63d6d8cdc69f', // 蒋琬,蜀,歩,3
         '5abed51fa17562728f0b54288c547c56', // 朱儁,漢,弓,4
@@ -103,14 +98,8 @@ describe('Routes: `/c`', () => {
     });
 
     it('retrieve by `army` filter', async () => {
-      const options = {
-        qs: {
-          filter: {
-            army: ['歩'],
-          },
-        },
-        json: true,
-      };
+      query.filter.army = ['歩'];
+      const options = { qs: query, json: true };
       const expectedIds = [
         '0022cae0ffb0ee3d8fce63d6d8cdc69f', // 蒋琬,蜀,歩,3
         'e0f015ef64ca6eef2ed4ad5debcd3fde', // S2陸遜,呉,歩,5
@@ -127,14 +116,8 @@ describe('Routes: `/c`', () => {
     });
 
     it('retrieve by `team` filter', async () => {
-      const options = {
-        qs: {
-          filter: {
-            team: ['呉'],
-          },
-        },
-        json: true,
-      };
+      query.filter.team = ['呉'];
+      const options = { qs: query, json: true };
       const expectedIds = [
         '30f401ff9134eb74663697174aa3ff10', // 周瑜,呉,弓,5
         'e0f015ef64ca6eef2ed4ad5debcd3fde', // S2陸遜,呉,歩,5
@@ -148,6 +131,40 @@ describe('Routes: `/c`', () => {
           )
         )
       );
+    });
+
+    describe('returning `[]` when no matching', () => {
+      it('no match `text`', async () => {
+        query.text = 'no match';
+        const options = { qs: query, json: true };
+        const subjects = await server.get('/c', options);
+        expect(subjects).toHaveLength(0);
+        expect(subjects).toEqual([]);
+      });
+
+      it('no match `filter.rarity`', async () => {
+        query.filter.rarity = [];
+        const options = { qs: query, json: true };
+        const subjects = await server.get('/c', options);
+        expect(subjects).toHaveLength(0);
+        expect(subjects).toEqual([]);
+      });
+
+      it('no match `filter.army`', async () => {
+        query.filter.army = [];
+        const options = { qs: query, json: true };
+        const subjects = await server.get('/c', options);
+        expect(subjects).toHaveLength(0);
+        expect(subjects).toEqual([]);
+      });
+
+      it('no match `filter.team`', async () => {
+        query.filter.team = [];
+        const options = { qs: query, json: true };
+        const subjects = await server.get('/c', options);
+        expect(subjects).toHaveLength(0);
+        expect(subjects).toEqual([]);
+      });
     });
   });
 });
--- server/routes/api/commander.js
+++ server/routes/api/commander.js
@@ -3,7 +3,7 @@ const qs = require('qs');
 const setApiHeaders = require('../helpers/setApiHeaders');
 const Commander = require('../../models/Commander');
 
-const search = async (req, res) => {
+const search = (req, res) => {
   setApiHeaders(res);
   const { text, filter } = qs.parse(req.query);
   let query = Commander;
@@ -11,15 +11,15 @@ const search = async (req, res) => {
     query = query.where('name').regex(text);
   }
   if (filter != null) {
-    query = Object.entries(filter).reduce(
-      (currentQuery, [key, array]) => currentQuery.where(key).in(array),
+    const filterKeys = ['rarity', 'army', 'team'];
+    query = filterKeys.reduce(
+      (currentQuery, key) => currentQuery.where(key).in(filter[key] || []),
       query
     );
   }
-  const commanders = query.select([
+  return query.select([
     '_id', 'identifier', 'name', 'imageURL',
   ]).find();
-  return commanders;
 };
 
 module.exports = {
