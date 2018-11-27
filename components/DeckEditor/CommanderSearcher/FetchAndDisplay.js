faa4376f03d591bcdbb85c05385efc6669fbd77d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-11-27 15:36:48 +0900

    adjust card height by viewport width

--- components/DeckEditor/CommanderSearcher/FetchAndDisplay.js
+++ components/DeckEditor/CommanderSearcher/FetchAndDisplay.js
@@ -4,6 +4,7 @@ import isEqual from 'lodash.isequal';
 
 // material-ui components
 import { withStyles } from '@material-ui/core/styles';
+import RootRef from '@material-ui/core/RootRef';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
@@ -20,7 +21,7 @@ const styles = theme => ({
 export class FetchAndDisplayComponent extends Component {
   state = {
     commanders: null,
-  }
+  };
 
   static getDerivedStateFromProps = (nextProps, prevState) => {
     if (!isEqual(nextProps.query, prevState.prevQuery)) {
@@ -30,12 +31,12 @@ export class FetchAndDisplayComponent extends Component {
       };
     }
     return null;
-  }
+  };
 
   componentDidMount = () => {
     const { query } = this.props;
     this.fetch(query);
-  }
+  };
 
   componentDidUpdate = () => {
     const { commanders } = this.state;
@@ -43,7 +44,8 @@ export class FetchAndDisplayComponent extends Component {
       const { query } = this.props;
       this.fetch(query);
     }
-  }
+    this.setDimensionVars(this.gridListRef);
+  };
 
   render = () => {
     const { commanders } = this.state;
@@ -52,11 +54,19 @@ export class FetchAndDisplayComponent extends Component {
       return <div>Loading...</div>;
     }
     return (
-      <GridList cols={3} spacing={2} cellHeight="auto" className={classes.root}>
-        {commanders.map(buildGridListTile(classes))}
-      </GridList>
+      <RootRef rootRef={this.setGridListRef}>
+        <GridList
+          cols={3}
+          spacing={2}
+          cellHeight="auto"
+          className={classes.root}>
+          {commanders.map(buildGridListTile(classes))}
+        </GridList>
+      </RootRef>
     );
-  }
+  };
+
+  setGridListRef = (node) => { this.gridListRef = node; };
 
   fetch = async (query) => {
     const uri = `/api/v1/c?${qs.stringify(query)}`;
@@ -67,12 +77,29 @@ export class FetchAndDisplayComponent extends Component {
     const commanders = await response.json();
     this.setState({ commanders });
   };
+
+  setDimensionVars = (element) => {
+    if (element != null) {
+      const ieh = element.clientHeight;
+      const iew = element.clientWidth;
+      element.style.setProperty('--ieh', `${ieh}px`);
+      element.style.setProperty('--iew', `${iew}px`);
+      this.setCardHeightVars(element, iew);
+    }
+  };
+
+  setCardHeightVars = (element, width) => {
+    if (element != null) {
+      const cardHeight = ((width - 6) / 3) * 1.371345029239765;
+      element.style.setProperty('--ch', `${cardHeight}px`);
+    }
+  };
 }
 
 export default withStyles(styles)(FetchAndDisplayComponent);
 
 const buildGridListTile = classes => c => (
-  <GridListTile key={c._id} style={{ height: '38%' }}>
+  <GridListTile key={c._id} style={{ height: 'var(--ch)' }}>
     <img src={c.imageURL} alt={c.id} />
     <GridListTileBar
       title={`${c.name}${c.special || ''}`}
