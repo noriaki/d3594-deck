b29a620208242c9992ae37eda45e5efbb52868dc
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-12-18 18:29:50 +0900

    refine styles of icon centering

--- components/DeckEditor/Stage/Commander/AddTactics.js
+++ components/DeckEditor/Stage/Commander/AddTactics.js
@@ -13,15 +13,24 @@ const styles = {
   root: {
     position: 'absolute',
     top: 0,
+    color: 'rgba(233,233,233,1)',
+    padding: 0,
+  },
+  label: {
+    display: 'block',
+    position: 'relative',
+    paddingTop: '100%',
+    overflow: 'hidden',
+  },
+  iconWrap: {
+    position: 'absolute',
+    top: 0,
     right: 0,
     bottom: 0,
     left: 0,
-    color: 'rgba(233,233,233,1)',
-    padding: 0,
-    display: 'unset',
-  },
-  label: {
-    height: 'calc(100% - .75rem)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   icon: {
     fontSize: '3rem',
@@ -32,7 +41,9 @@ export const AddTacticsComponent = ({ classes, onClick: handleClick }) => {
   const { root, label } = classes;
   return (
     <Button fullWidth classes={{ root, label }} onClick={handleClick}>
-      <AddIcon className={classes.icon} />
+      <span className={classes.iconWrap}>
+        <AddIcon className={classes.icon} />
+      </span>
     </Button>
   );
 };
