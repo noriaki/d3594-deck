0ab7d381bbdf2d4babfb7947a3bacce33ace5aed
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-01-16 22:24:09 +0900

    add load local data

--- scripts/importData.js
+++ scripts/importData.js
@@ -1,31 +1,60 @@
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
 const fetch = require('isomorphic-fetch');
 const { connect, disconnect } = require('../server/db');
 const Commander = require('../server/models/Commander');
 const Tactics = require('../server/models/Tactics');
+const Formation = require('../server/models/Formation');
 
 const logAndExit = (error) => { console.error(error); process.exit(1); };
 
-const isValidEnv = () => {
+const getValidEnv = () => {
   const { NODE_ENV, DATA_REPO } = process.env;
-  return (
-    NODE_ENV != null && NODE_ENV === 'production' && DATA_REPO != null
-  );
+  let ret = false;
+  if (NODE_ENV != null) {
+    if (NODE_ENV === 'production' && DATA_REPO != null) {
+      ret = 'remote';
+    } else if (NODE_ENV === 'development') {
+      ret = 'local';
+    }
+  }
+  return ret;
 };
 
 // async
 const getAllData = () => {
-  const { DATA_REPO } = process.env;
-  const uri = `https://github.com/${DATA_REPO}/raw/master/data/commanders.json`;
+  const env = getValidEnv();
+  if (env === 'remote') {
+    const { DATA_REPO } = process.env;
+    return getAllDataRemote(DATA_REPO);
+  } else if (env === 'local') {
+    return Promise.resolve(getAllDataLocal());
+  }
+  // never reach
+  return new Error('InValid Environment');
+};
+
+// async
+const getAllDataRemote = (repo) => {
+  const uri = `https://github.com/${repo}/raw/master/data/commanders.json`;
   return fetch(uri, {
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
   }).then(response => response.json());
 };
 
+const getAllDataLocal = () => {
+  const path = './data/commanders.json';
+  return JSON.parse(readFileSync(resolve(path), 'utf8'));
+};
+
 const main = async () => {
-  if (!isValidEnv()) { throw new Error('InValid Environment'); }
+  if (getValidEnv() === false) { throw new Error('InValid Environment'); }
 
   // setup
   await connect();
+  if (getValidEnv() === 'local') {
+    await Formation.deleteMany({});
+  }
   await Tactics.deleteMany({});
   await Commander.deleteMany({});
 
@@ -33,6 +62,9 @@ const main = async () => {
 
   await Commander.importAll(data);
   await Tactics.importAll(data);
+  if (getValidEnv() === 'local') {
+    await Formation.importSampleData();
+  }
 
   // teardown
   await disconnect();
