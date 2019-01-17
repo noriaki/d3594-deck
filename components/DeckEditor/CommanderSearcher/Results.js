ac061745bcb7d0dab8e148d3a956a817dcdb10ef
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-01-17 21:11:09 +0900

    add touch{start,move,end,cancel} events of scroll managing

--- components/DeckEditor/CommanderSearcher/Results.js
+++ components/DeckEditor/CommanderSearcher/Results.js
@@ -23,8 +23,46 @@ export class ResultsComponent extends Component {
     this.setDimensionVars(this.gridListRef);
   }
 
+  componentDidMount = () => {
+    this.listenTouchStart();
+  };
+
   componentDidUpdate = () => {
     this.setDimensionVars(this.gridListRef);
+  };
+
+  componentWillUnmount = () => {
+    this.removeTouchStart();
+  };
+
+  listenTouchStart = () => {
+    this.gridListRef.addEventListener('touchstart', this.handleTouchStart);
+  };
+
+  removeTouchStart = () => {
+    this.gridListRef.removeEventListener('touchstart', this.handleTouchStart);
+  };
+
+  handleTouchStart = () => {
+    this.gridListRef.addEventListener(
+      'touchmove', this.handleTouchMove, { passive: false }
+    );
+    this.gridListRef.addEventListener('touchend', this.handleTouchEnd);
+    this.gridListRef.addEventListener('touchcancel', this.handleTouchEnd);
+  };
+
+  handleTouchMove = (event) => { event.preventDefault(); };
+
+  handleTouchEnd = () => {
+    this.removeTouchListeners();
+  };
+
+  removeTouchListeners = () => {
+    this.gridListRef.removeEventListener(
+      'touchmove', this.handleTouchMove, { passive: false }
+    );
+    this.gridListRef.removeEventListener('touchend', this.handleTouchEnd);
+    this.gridListRef.removeEventListener('touchcancel', this.handleTouchEnd);
   };
 
   render = () => {
