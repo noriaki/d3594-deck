6c034f3df66cec5c1c358cea6fcb158b6c7e2345
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-02-10 16:57:58 +0900

    to externalize sample formation data

--- server/models/Formation.js
+++ server/models/Formation.js
@@ -3,6 +3,7 @@ const mongooseAutoPopulatePlugin = require('mongoose-autopopulate');
 const { toIdFromInstance } = require('./concerns/identify');
 const LearnedCommander = require('./LearnedCommander');
 const FormationClass = require('./classes/Formation');
+const sampleFormationData = require('../../data/init/formations/sample');
 
 const { Schema } = mongoose;
 
@@ -52,34 +53,17 @@ formationSchema.static('fetchById', fetchById);
 
 
 async function importSampleData() {
-  const commanderIds = [
-    'e0f015ef64ca6eef2ed4ad5debcd3fde', // S2陸遜
-    '30f401ff9134eb74663697174aa3ff10', // 周瑜
-    '29fb183da598388eee0cd2f73832de8e', // 呂蒙
-  ];
-  const tacticsIds = [
-    [
-      '3f5a7a77ecd8df99438d1faf9221d5e0', // 不攻
-      '57ab3a19350045df5dfcf65ed187792a', // 十面埋伏
-    ],
-    [
-      '270f3fbedde0140b6c679b17d79df385', // 水淹七軍
-      'e4dedbfa9c3109173f166c83c4f8e5d6', // 渾水摸魚
-    ],
-    [
-      '967e6d8582235fe4682e46708c6f0752', // 掎角之勢
-      'c44f950c80d01601ec8bf31f75d89a40', // 反計之策
-    ],
-  ];
-  const commanders = await Promise.all(commanderIds.map(
-    (commanderId, i) => (
-      LearnedCommander.createAssociation(commanderId, tacticsIds[i])
+  const commanders = await Promise.all(sampleFormationData.formation.map(
+    ({ commander, tactics }) => (
+      LearnedCommander.createAssociation(commander, tactics)
     )
   ));
   // /f/43e0f069ab00049908ab34390a9c45ca
   await this.createAssociation(commanders, '大都督（呉レンジャー）');
+
+  const immatureData = sampleFormationData.immatureCommander;
   const immatureCommander = await LearnedCommander.createAssociation(
-    commanderIds[1], [null, tacticsIds[1][1]]
+    immatureData.commander, immatureData.tactics
   );
   // /f/8f112238f2392424f26f740360629aae
   await this.createAssociation(
