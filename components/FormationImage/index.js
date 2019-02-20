67835708e3d9c9b5af713390c098561e78802306
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-02-20 17:54:57 +0900

    add syncing canvas and img

--- components/FormationImage/index.js
+++ components/FormationImage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useEffect, useRef } from 'react';
 
 // canvas (konva)
 import {
@@ -14,30 +14,37 @@ import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({});
 
-export const FormationImageComponent = ({ classes }) => (
-  <Paper square>
-    <Stage width={300} height={200} onClick={handleClickToDownload} onTap={handleClickToDownload}>
-      <Layer>
-        <Text text="TTT Canvas" />
-        <Rect
-          x={20}
-          y={20}
-          width={50}
-          height={50}
-          fill="#fff"
-          shadowBlur={5} />
-      </Layer>
-    </Stage>
-  </Paper>
-);
+export const FormationImageComponent = ({ classes }) => {
+  const canvasRef = useRef(null);
+  const imageRef = useRef(null);
+  useEffect(() => {
+    if (canvasRef.current && imageRef.current) {
+      const data = canvasRef.current.toDataURL();
+      imageRef.current.src = data;
+    }
+  });
+
+  return (
+    <Fragment>
+      <Paper square>
+        <Stage width={300} height={200} ref={canvasRef}>
+          <Layer>
+            <Text text="TTT Canvas" />
+            <Rect
+              x={20}
+              y={20}
+              width={50}
+              height={50}
+              fill="#fff"
+              shadowBlur={5} />
+          </Layer>
+        </Stage>
+      </Paper>
+      <Paper>
+        <img alt="canvas" width={300} height={200} ref={imageRef} />
+      </Paper>
+    </Fragment>
+  );
+};
 
 export default withStyles(styles)(FormationImageComponent);
-
-const handleClickToDownload = (event) => {
-  const mimeTyep = 'image/jpeg';
-  const data = event.target.toDataURL({ mimeTyep, quality: 0.8 });
-  const link = document.createElement('a');
-  link.href = data.replace(mimeTyep, 'image/octet-stream');
-  link.download = 'file.jpg';
-  link.click();
-};
