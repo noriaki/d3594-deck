c70154d9afaf737e4c12ae826a5a068f30020788
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-03-01 12:07:42 +0900

    refactor file move

--- components/MetaTags/index.js
+++ components/MetaTags/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 
 // constants
-import { host as assetHost } from '../constants/assets';
+import { host as assetHost } from '../../constants/assets';
 
 const title = '大三国志 部隊編成アプリ';
 const description = 'ゲーム｢大三国志｣の部隊を自由に編成して共有できるアプリ。分城を増やさなくても、校場や統帥庁のLvを上げなくても、 好きなだけ部隊が組める。 経験値を気にせず戦法を付け外しできる。 そんな夢のようなアプリ、できました。';
